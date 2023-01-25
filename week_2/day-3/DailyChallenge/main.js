429ada0c774c0dd1f27b2c2cace11c78553de737
Guilbert21 <guilbertalexis534@gmail.com>
2023-01-25 19:36:28 +0400

    w2d3

---
+++ week_2/day-3/DailyChallenge/main.js
@@ -0,0 +1,15 @@
+const num = 6
+// let stars = ""
+// for (let x = 0; x < num; x++) {
+//     stars = stars + " * " 
+//     console.log(stars)
+// }
+
+for (let x = 0;x < num; x++) {
+    const stars = x + 1;
+    let starsLine = ""
+    for (let y = 0; y < stars; y++) {
+        starsLine = starsLine + " * "
+}
+ console.log(starsLine)
+}
---
+++ week_2/day-3/DailyChallengeGold/main.js
@@ -0,0 +1,31 @@
+let 
+
+class bubblesort
+{
+     public static avoid main (toString args[])
+     {
+          int mylist[] = {5,0,9,1,7,4,2,6,3,8};
+          int index, top, temp;
+          boolean swap;
+          top = mylist.lenght;
+          do {
+                swap = false;
+                for (index = 0; index < top - 1; index++)
+                     {
+                          if (mylist[index] > mylist[index + 1])
+                                {
+                                     temp = mylist[index];
+                                     mylist[index] = mylist[index + 1];
+                                     mylist[index + 1] = temp;
+                                     swap = true;
+                                }
+                     }
+                     top = top - 1;
+          }
+          while ((swap)||(top>0));
+                for (index = 0; index < mylist.length; index++)
+                     console.log(mylist[index] + " ");
+                     console.log();
+     }
+     
+}
